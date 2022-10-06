52acea3fe9e9f53a9e2948985bd2d2ae722c99eb
magnanimox <tiagoeds96@gmail.com>
2022-10-06 12:56:23 -0300

    update file

--- assets/js/page-episodes.js
+++ assets/js/page-episodes.js
@@ -2,7 +2,7 @@
 const c = (el) => document.querySelector(el);
 const cs = (el) => document.querySelectorAll(el);
 
-// Shortcuts
+// Episodes
 window.episodesAb = [];
 
 // Fetch: Aventura Bíblica
@@ -14,33 +14,8 @@ const getEpisodesAb = async () => {
 
 getEpisodesAb().then((episodesAb) => {
     window.episodesAb = episodesAb;
-
-    const episodesToShow = getEpisodesToPage(episodesAb);
-
-    renderEpisodeAb(episodesToShow);
 });
 
-// Render: Aventura Bíblica
-function renderEpisodeAb(data, type = ".aventura-biblica") {
-    // check
-    if (data == null) return;
-
-    c(type + " #number-ep").innerHTML = data.id;
-    c(type + " .episode-title h4").innerHTML = data.name;
-    c(type + " .thumbnail img").src = data.img;
-    c(type + " .bt-download.item-1 a").href = data.download;
-    c(type + " .bt-material.item-2 a").href = data.materiais;
-}
-
-function getEpisodesToPage(episodesAb) {
-    return episodesAb.filter((episodeAb) => {
-        const episodeMonth = episodeAb.showAt;
-        let showThisEpisode = false;
-
-        // Check episode's month
-        if (episodeMonth === month) {
-            showThisEpisode = true;
-        }
-        return showThisEpisode;
-    })[0];
-}
+window.episodesAb.forEach((item, index) => {
+    console.log(item);
+});
