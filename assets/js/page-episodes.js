13d48ec3c732c908865a75e5f24e901012e0d3ff
magnanimox <tiagoeds96@gmail.com>
2022-10-06 17:15:14 -0300

    updating

--- assets/js/page-episodes.js
+++ assets/js/page-episodes.js
@@ -1,7 +1,3 @@
-// Universal Shortcuts
-const c = (el) => document.querySelector(el);
-const cs = (el) => document.querySelectorAll(el);
-
 // Episodes
 window.episodesAb = [];
 window.episodesTp = [];
