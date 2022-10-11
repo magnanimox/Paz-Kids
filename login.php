ee68a8f0eee26b39684af962acdae069badac353
magnanimox <tiagoeds96@gmail.com>
2022-10-11 14:27:51 -0300

    update login.php

--- login.php
+++ login.php
@@ -1,7 +1,18 @@
 <?php
 include("conexao.php");
 
-if(empty($_POST["usuario"]) || empty($_POST["senha"])) {
+if(empty($_POST["user"]) || empty($_POST["password"])) {
     header("Location: index.html");
     exit();
 }
+
+$user = mysqli_real_escape_string($conexao, $_POST["user"]);
+$password = mysqli_real_escape_string($conexao, $_POST["password"]);
+
+$query = "select user_id, user from usuario where user = {$user} and password = md5({$password})";
+
+$result = mysqli_query($conexao, $query);
+
+$row  = mysqli_num_rows($result);
+
+echo $row;exit;
