2eda72fef3f7194033cd251321d333cbd4d9d19e
magnanimox <tiagoeds96@gmail.com>
2022-10-12 14:12:05 -0300

    fix some terms SQL

--- login.php
+++ login.php
@@ -9,7 +9,7 @@ if(empty($_POST["user"]) || empty($_POST["password"])) {
 $user = mysqli_real_escape_string($conexao, $_POST["user"]);
 $password = mysqli_real_escape_string($conexao, $_POST["password"]);
 
-$query = "select user_id, user from usuario where user = {$user} and password = md5({$password})";
+$query = "select user from usuario where user = '{$user}' and password = md5('{$password}')";
 
 $result = mysqli_query($conexao, $query);
 
