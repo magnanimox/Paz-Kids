704490b37220b933c71598275ded9ac75d329166
magnanimox <tiagoeds96@gmail.com>
2022-10-12 14:30:07 -0300

    fix some terms SQL

--- login.php
+++ login.php
@@ -15,7 +15,7 @@ $result = mysqli_query($conexao, $query);
 
 $row  = mysqli_num_rows($result);
 
-if (row == 1){
+if ($row == 1){
     $_SESSION["user"] = $user;
     header("Location: painel.php");
     exit();
