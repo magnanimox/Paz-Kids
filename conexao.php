d1588c36e6e92d3b82c0809c86d21fb9b6bc17ad
magnanimox <tiagoeds96@gmail.com>
2022-10-18 12:37:51 -0300

    update

--- conexao.php
+++
@@ -1,7 +0,0 @@
-<?php
-define("HOST", "ns964.hostgator.com.br");
-define("USUARIO", "pazkid16_magnanimox");
-define("SENHA", "VemMax131296");
-define("DB", "pazkid16_login");
-
-$conexao = mysqli_connect(HOST, USUARIO, SENHA, DB) or die ("Não foi possível conectar");
